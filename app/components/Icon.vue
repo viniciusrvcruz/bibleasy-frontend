817994d3f0ad4e44e910c0616173378e5cea5883
Vinicius Cruz <viniciusrobertopro@gmail.com>
2025-12-22 15:25:35 -0300

    feat: create icon component

---
+++ app/components/Icon.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+defineProps<{
+  icon: keyof typeof ICON_MAP
+  size?: number
+}>()
+
+const ICON_MAP = {
+  circle_question: 'lucide:circle-question-mark',
+  book: 'mdi:book-open-outline',
+  document: 'mdi:file-document-outline',
+  clock: 'mdi:clock-outline',
+  edit: 'mdi:pencil',
+  help: 'mdi:help-circle-outline',
+  palette: 'mdi:palette-outline',
+  menu: 'mdi:menu',
+  user: 'lucide:user',
+  book_marked: 'lucide:book-marked',
+} as const
+
+</script>
+
+<template>
+  <NuxtIcon :name="ICON_MAP[icon]" :size="size" />
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,8 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/icon"],
+
+  icon: {
+    componentName: 'NuxtIcon'
+  }
 })
